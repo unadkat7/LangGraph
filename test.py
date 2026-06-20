bd976e0621adcfc34431fae2b8d5fde082321d82
unadkat7 <153261041+unadkat7@users.noreply.github.com>
2026-06-20 12:49:14 +0530

    fix:

---
+++ test.py
@@ -0,0 +1,10 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash"
+)
+
+print(model.invoke("Hello").content)
